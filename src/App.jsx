b4802da8fcb4abc52ed8d9eb5ba08a912d0fb609
eslam <eslam@hp.debian>
2025-07-16 16:10:47 +0300

    change BrowserRouter to Hashrouter for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
